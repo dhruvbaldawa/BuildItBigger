01581f877f5ec610a730966387746218d68217d0
Dhruv Baldawa <dhruvbaldawa@gmail.com>
2016-06-13 02:23:56 +0530

    Make a proper joke returning backend endpoint

--- jokeprovider/src/main/java/com/dhruvb/jokeprovider/JokeProvider.java
+++ jokeprovider/src/main/java/com/dhruvb/jokeprovider/JokeProvider.java
@@ -1,10 +1,18 @@
 package com.dhruvb.jokeprovider;
 
+import java.util.Random;
+
 /**
  * Created by dhruv on 24/5/16.
  */
 public class JokeProvider {
+    final private String[] ALL_JOKES = {
+            "If you work in an office with Chuck Norris, don't ask him for his three-hole-punch.",
+            "When Chuck Norris wants an egg, he cracks open a chicken.",
+            "Chuck Norris can access private methods.",
+            "Chuck Norris can believe it's not butter.",
+    };
     public String getJoke() {
-        return "Joke!";
+        return ALL_JOKES[new Random().nextInt(ALL_JOKES.length)];
     }
 }
